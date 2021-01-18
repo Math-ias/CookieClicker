8223bf038f27184a485f7c2341a17db634788410
Math-ias <kools.m@northeastern.edu>
2021-01-17 21:30:19 -0500

    Remove golden cookie mention in package-info.java.
    I'd prefer to consolidate mentioning this somewhere else than
    in this package.

--- src/main/java/com/cookie/assets20291M/package-info.java
+++ src/main/java/com/cookie/assets20291M/package-info.java
@@ -1,7 +1,6 @@
 /**
  * This package contains the classes necessary to bake up to one million cookies in version 2.029 of
  * CookieClicker. It includes all the possible upgrades, and buildings reasonably obtainable on the
- * route to the first one million cookies. This does not include golden cookie effects for now
- * because of their complexity.
+ * route to the first one million cookies.
  */
 package com.cookie.assets20291M;
